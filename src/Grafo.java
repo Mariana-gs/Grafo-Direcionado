d2a7dafc6c7fd22bb6f5cba01dd8fee414c38cdb
Mariana Galvão <81964220+Mariana-gs@users.noreply.github.com>
2022-08-30 14:52:03 -0300

    Obtendo Dados

--- src/Grafo.java
+++ src/Grafo.java
@@ -2,28 +2,32 @@ import java.lang.reflect.Constructor;
 
 public class Grafo {
 
-    private int grafo[][];
+    private int grafo[];
     private int qtdVertices;
     private int qtdArestas;
 
+    private void criarGrafo(String arquivoGrafo){
+        String entrada;
+        String dados[];
+
+        ArquivoLeitura arquivo = new ArquivoLeitura(arquivoGrafo);
+        boolean erro = true;
+
+        entrada = arquivo.Ler();
+        dados = entrada.split("  ");
+        this.qtdVertices = Integer.parseInt(dados[0]);
+        this.qtdArestas = Integer.parseInt(dados[1]);
+
+        
+    }
 
     Grafo(String arquivoGrafo){
-        ArquivoLeitura arquivo = new ArquivoLeitura(arquivoGrafo);
 
+        criarGrafo(arquivoGrafo);
 
+        System.out.println(this.qtdVertices + " - " + this.qtdArestas);
     }
 
-
-
-    int grauSaida(int vertice){
-
-        return 0;
-    }
-
-    int grauEntrada(int vertice){
-
-        return 0;
-    }
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,14 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner e = new Scanner(System.in);
+
+        System.out.println("===== Implementação de Grafos Direcionados =====");
+        System.out.println("Informe o Caminho do Arquivo com os Dados do Grafo:" );
 
 
-
-
+        String caminho = e.nextLine();
+        Grafo grafo = new Grafo(caminho);
 
 
     }
