72ec29209cbcd84265e75f5af5d6479fbee04486
Mariana Galvão <81964220+Mariana-gs@users.noreply.github.com>
2022-09-01 13:21:04 -0300

    Tratando Erros de Caminho do Arquivo

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,51 @@ import java.util.Scanner;
 
 public class Main {
 
+    static public void executarPrograma(){
+        Scanner e = new Scanner(System.in);
+        boolean excecao = true;
+        String caminho;
+
+        System.out.println("\n == Abrir Arquivo ==");
+        System.out.println(" Informe o Caminho do Arquivo com os Dados do Grafo:" );
+
+        while (excecao) {
+            caminho = e.nextLine();
+            try {
+                Grafo grafo = new Grafo(caminho);
+                excecao = false;
+            } catch (Exception E) {
+                System.out.println(" Não foi Possível Abrir o Arquivo!");
+                System.out.println(" Por Favor Informe o Caminho Novamente: ");
+            }
+        }
+
+        
+    }
+
 
     public static void main(String[] args) {
+
         Scanner e = new Scanner(System.in);
+        int menuUm = 0;
 
-        System.out.println("\n===== Implementação de Grafos Direcionados =====");
-        System.out.println("Informe o Caminho do Arquivo com os Dados do Grafo:" );
+        while (menuUm != 1 && menuUm != 2) {
+            System.out.println("\n===== Implementação de Grafos Direcionados =====");
+            System.out.println(" == Menu Principal ==");
+            System.out.println(" 1. Abrir Arquivo ");
+            System.out.println(" 2. Sair ");
+            menuUm = e.nextInt();
+        }
 
-
-        //String caminho = e.nextLine();
-        String caminho = "D:\\GitHub\\Grafo Direcionado\\src\\Arquivos\\graph-test-4.txt";
-        Grafo grafo = new Grafo(caminho);
-
-
-        grafo.exibirSucessores(1);
-        grafo.exibirPredecessores(2);
-        grafo.grauSaida(2);
-        grafo.grauEntrada(2);
-
-
+        switch (menuUm){
+            case 1:
+                executarPrograma();
+                break;
+            case 2:
+                System.out.println("Programa Finalizado!");
+                e.close();
+                break;
+        }
 
     }
 }
