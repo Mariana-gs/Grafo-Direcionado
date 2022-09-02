75e07744f1ba3de33382e3ea9d027964a2acdce7
Mariana Galvão <81964220+Mariana-gs@users.noreply.github.com>
2022-09-02 16:55:05 -0300

    Repetição Menu

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,28 @@
 import java.util.Scanner;
 
 public class Main {
+    static public Scanner e = new Scanner(System.in);
 
-    static public void executarPrograma(){
-        Scanner e = new Scanner(System.in);
-        String caminho = "";
-        int vertice = 0;
-        boolean excecao = true;
+
+    static int menuDois(){
+        System.out.println("\nO que deseja fazer agora? ");
+        System.out.println("1. Informar outro Vértice ");
+        System.out.println("2. Abrir outro Arquivo ");
+        System.out.println("3. Sair do Programa ");
+        return e.nextInt();
+    }
+
+
+    static public Grafo abrirArquivo(){
         Grafo grafo = null;
-
+        boolean excecao = true;
+        String caminho = "";
         System.out.println("\n == Abrir Arquivo ==");
-        System.out.println(" Informe o Caminho do Arquivo com os Dados do Grafo:" );
+        System.out.println(" Informe o Caminho do Arquivo com os Dados do Grafo:");
+        e.nextLine();
+        caminho = e.nextLine();
 
         while (excecao) {
-            caminho = e.nextLine();
             try {
                 grafo = new Grafo(caminho);
                 System.out.println(" Grafo criado com Sucesso!");
@@ -21,29 +30,41 @@ public class Main {
             } catch (Exception E) {
                 System.out.println(" Não foi Possível Abrir o Arquivo!");
                 System.out.println(" Por Favor Informe o Caminho Novamente: ");
+                excecao = true;
             }
         }
         System.out.println("\n O grafo informado possui: \n-> " + grafo.getQtdVertices() + " Vértices\n-> " + grafo.getQtdArestas() + " Arestas");
-        System.out.println(" Informe um vértice para continuar: ");
-        vertice = e.nextInt();
 
-        System.out.println("\nInformações sobre o vértice: ");
-        grafo.grauSaida(vertice);
-        grafo.grauEntrada(vertice);
-        grafo.exibirSucessores(vertice);
-        grafo.exibirPredecessores(vertice);
+        return grafo;
+    }
 
-        //menu 2
-        System.out.println("\nO que deseja fazer agora? ");
-        System.out.println("1. Informar outro Vértice ");
-        System.out.println("2. Abrir outro Arquivo ");
-        System.out.println("3. Sair do Programa ");
-        
+    static public void executarPrograma(){
+
+
+        int vertice = 0;
+        int menuDois = 0;
+        Grafo grafo = null;
+
+        do {
+            grafo = abrirArquivo();
+            do {
+                System.out.println(" Informe um vértice para continuar: ");
+                vertice = e.nextInt();
+                System.out.println("\nInformações sobre o vértice: ");
+                grafo.grauSaida(vertice);
+                grafo.grauEntrada(vertice);
+                grafo.exibirSucessores(vertice);
+                grafo.exibirPredecessores(vertice);
+
+                menuDois = menuDois();
+            }while (menuDois == 1);
+        }while (menuDois == 2);
+
+
     }
 
     public static void main(String[] args) {
 
-        Scanner e = new Scanner(System.in);
         int menuUm = 0;
 
         while (menuUm != 1 && menuUm != 2) {
